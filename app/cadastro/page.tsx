fd110d99e481446699511a2519f0c5aff68715c8
Lucas Hector <1lucashector@gmail.com>
2026-03-28 12:28:05 -0300

    fix: move force-dynamic after use client directive

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
-export const dynamic = 'force-dynamic';
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
